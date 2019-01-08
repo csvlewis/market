9f4683a0ad4b27071ff8bf6a9965ca58fdb840a1
Chris Lewis <13chlewis@gmail.com>
2019-01-08 09:44:06 -0700

    All iteration 3 market tests passing, iteration 3 complete

--- lib/market.rb
+++ lib/market.rb
@@ -17,4 +17,19 @@ class Market
   def vendors_that_sell(item)
     @vendors.find_all { |vendor| vendor.inventory.key?(item) }
   end
+
+  def sorted_item_list
+    all_items = @vendors.map { |vendor| vendor.inventory.keys}
+    all_items.flatten.uniq.sort
+  end
+
+  def total_inventory
+    total_inventory = Hash.new(0)
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, amount|
+        total_inventory[item] += amount
+      end
+    end
+    total_inventory
+  end
 end
